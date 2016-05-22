0b496ef12703958c3fae92b1c9eb0a9e4876a720
jordan <jordan.chou1@gmail.com>
2016-05-22 11:28:28 +0800

    Added comments

--- sliding_window.h
+++ sliding_window.h
@@ -27,6 +27,14 @@ typedef struct
 {
     int source;
     int dest;
+    int data[MAX_MESSAGE_SIZE];
+} Packet;
+
+
+typedef struct
+{
+    int source;
+    int dest;
     KIND kind;
     size_t length;
     int checksum;
@@ -52,24 +60,43 @@ void timeout_link_3(CnetEvent ev, CnetTimerID timer, CnetData data);
 void timeout_link_4(CnetEvent ev, CnetTimerID timer, CnetData data);
 void print_window(int link);
 
+//Array to store the timers, one for each link, max of 4 links
 static CnetTimerID timers[4][WINDOW_SIZE+1];
+
+//Contains the names of each node
 static char* nodes[7] = {"Australia", "Fiji","New Zealand","Indonesia","Singapore","Malaysia","Brunei"};
+
+//The number of messages in the window. One for each link
 static int num_in_window[4] = {0,0,0,0};
+
+//The number of messages in store-forward buffer, one for each link
 static int num_in_buffer[4] = {0,0,0,0};
+
+//The frames stored in each window
 static Frame window[4][WINDOW_SIZE + 1];
+
+//The bounds of the store-forward buffer
 static int buffer_bounds[4][3] = {{0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}};
+
+//The frames stored in the store-forward buffer
 static Frame buffer[4][WINDOW_SIZE+1];
+
+//The frame expected for each link
 static int frame_expected[4] = {0,0,0,0};
+
+//The ack expected from each link
 static int ack_expected[4] = {0,0,0,0};
+
+//The next frame expected from each link
 static int next_frame[4] = {0,0,0,0};
 
 static int routing_table[7][7] =
-      {{0,1,2,3,3,3,3},   //AUSTRALIA
-       {1,0,1,1,1,1,1},   //FIJI
-       {1,1,0,1,1,1,1},   //NEW ZEALAND
-       {1,1,1,0,2,3,4},   //INDONESIA
-       {1,1,1,1,0,2,1},   //SINGAPORE
-       {1,1,1,1,2,0,1},   //MALAYSIA
-       {1,1,1,1,1,1,0}};  //BRUNEI
+      {{0,1,2,3,3,3,3},   //Australia
+       {1,0,1,1,1,1,1},   //Fiji
+       {1,1,0,1,1,1,1},   //NewZealand
+       {1,1,1,0,2,3,4},   //Indonesia
+       {1,1,1,1,0,2,1},   //Singapore
+       {1,1,1,1,2,0,1},   //Malaysia
+       {1,1,1,1,1,1,0}};  //BruneiB
 
 #endif
